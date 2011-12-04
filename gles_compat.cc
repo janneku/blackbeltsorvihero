83041dc1a597e11d49e5eb498b05129bac53bc6a
Janne Kulmala <janne.t.kulmala@tut.fi>
2011-12-05 01:54:09 +0200

    gles_compat: Add SGI copyright notice [DOCUMENTATION]

--- gles_compat.cc
+++ gles_compat.cc
@@ -7,7 +7,24 @@
  * Program code and resources are licensed with GNU LGPL 2.1. See
  * COPYING.LGPL file.
  *
+ * Contains portions from SGI with the following license:
  *
+ * SGI FREE SOFTWARE LICENSE B (Version 2.0, Sept. 18, 2008)
+ * Copyright (C) 1991-2000 Silicon Graphics, Inc. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice including the dates of first publication and
+ * either this permission notice or a reference to
+ * http://oss.sgi.com/projects/FreeB/
+ * shall be included in all copies or substantial portions of the Software.
+ *
+ * Description:
  * OpenGL compability for GLES
  */
 #include "gl.h"
